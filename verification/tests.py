e28a47ecbda67cc2621a1360e764a455bc9bdd90
DahliaSR <Dahlia.S.Ramm@gmail.com>
2017-09-18 23:37:09 +0200

    added explanations to tests

--- verification/tests.py
+++ verification/tests.py
@@ -1,9 +1,10 @@
 """
 TESTS is a dict with all you tests.
+
 Keys for this will be categories' names.
-Each test is dict with
+Each test is a dict with:
     "input" -- input data for user function
-    "answer" -- your right answer
+    "answer" -- the expected answer
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
@@ -12,26 +13,37 @@ TESTS = {
     "Basics": [
         {
             "input": "sdsffffse",
-            "answer": 4
+            "answer": 4,
+            "explanation": "'ffff'"
         },
         {
             "input": "ddvvrwwwrggg",
-            "answer": 3
+            "answer": 3,
+            "explanation": "'www' and 'ggg' have a length of 3"
         }
     ],
     "Extra": [
         {
             "input": "",
-            "answer": 0
-        },{
-            "input": "abababaab",
-            "answer": 2
-        },{
-            "input": "abababa",
-            "answer": 1
-        },{
+            "answer": 0,
+            "explanation":
+                "The longest substring of an empty " +
+                "string can only have a length of 0"
+        },
+        {
+            "input": "'abababaab'",
+            "answer": 2,
+            "explanation": "'aa'"
+        },
+        {
+            "input": "'abababa'",
+            "answer": 1,
+            "explanation": "'a' and 'b' have a length of 1"
+        },
+        {
             "input": "aa",
-            "answer": 2
+            "answer": 2,
+            "explanation": "'aa'"
         }
     ]
 }
